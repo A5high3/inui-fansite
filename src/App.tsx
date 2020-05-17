a21a66215ff3434622ff364778a64a300f419430
ashgray <7ashgray7@gmail.com>
2020-05-17 18:50:15 +0900

    動画仮作成。topに反映。第2セクションに立ち絵のみ反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import inui from "./Inui_Toko_body.png";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="App-header">
+          <div className="Top-hashtag">#いぬいどんどんすきになる</div>
+          <video autoPlay loop muted style={{ width: "100%" }}>
+            <source src={require("./inuimov.mp4")} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div className="App">
+        <header className="App-header2">
+          <img src={inui} />
+        </header>
+      </div>
+    </>
   );
 }
 
