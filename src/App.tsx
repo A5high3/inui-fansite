cab593f16a87da4c05becee40f4d4c15f89aff98
ashgray <7ashgray7@gmail.com>
2020-09-05 14:32:00 +0900

    レスポンシブ仮対応

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,57 @@ import EmbededYoutubeMusic from "./components/embededYoutubeMusic";
 import EmbededYoutubeYozuri from "./components/embededYoutubeYoduri";
 import EmbededYoutubeTalk from "./components/embededYoutubeTalk";
 import EmbededTwitter from "./components/embededTwitter";
+import EmbededYoutubeGame from "./components/embededYoutubeGame";
 import Caution from "./components/caution";
 import Bonus from "./components/bonus";
+import MediaQuery from "react-responsive";
+import responsivetop from "./assets/images/responsivetop.jpeg";
 import "./App.css";
-import EmbededYoutubeGame from "./components/embededYoutubeGame"
 
 function App() {
   return (
-    <body style={{ cursor: "url(./bankencursol.png),auto" }}>
-      {/* トップヘッダ */}
-      <div className="App">
-        <div className="App-header">
-          <div className="Top-hashtag" />
-          <img src={inuitop} style={{ width: "100%", height: "100%" }} />
+    <>
+      <MediaQuery minDeviceWidth={1024}>
+        <body style={{ cursor: "url(./bankencursol.png),auto" }}>
+          {/* トップヘッダ */}
+          <div className="App">
+            <div className="App-header">
+              <div className="Top-hashtag" />
+              <img src={inuitop} style={{ width: "100%", height: "100%" }} />
+            </div>
+          </div>
+
+          {/* music埋め込みセクション */}
+          <EmbededYoutubeMusic />
+
+          {/* 夜釣り埋め込みセクション */}
+          <EmbededYoutubeYozuri />
+
+          {/* 雑談埋め込みセクション */}
+          <EmbededYoutubeTalk />
+
+          {/* ゲーム埋め込みセクション */}
+          <EmbededYoutubeGame />
+
+          {/* Twitter埋め込みセクション */}
+          <EmbededTwitter />
+
+          {/* ｱﾊｧ↑ボタン */}
+          <Bonus />
+
+          {/* 注釈 */}
+          <Caution />
+        </body>
+      </MediaQuery>
+      <MediaQuery minDeviceWidth={320} maxDeviceWidth={1023}>
+        {/* <h1>入り待ち出待ち雪待</h1> */}
+        <div style={{width: "auto", height:"auto"}}>
+          <div className="hogehoge">戌亥とこ非公式</div>
+          <div className="hogehoge2">ふぁんさいと</div>
+          <img src={responsivetop} />
         </div>
-      </div>
-
-      {/* youtube埋め込みセクション */}
-      <EmbededYoutubeMusic />
-
-      {/* youtube埋め込みセクション */}
-      <EmbededYoutubeYozuri />
-
-      <EmbededYoutubeTalk />
-
-      <EmbededYoutubeGame />
-
-      {/* Twitter埋め込みセクション */}
-      <EmbededTwitter />
-
-      {/* ｱﾊｧ↑ボタン */}
-      <Bonus />
-
-      {/* 注釈 */}
-      <Caution />
-    </body>
+      </MediaQuery>
+    </>
   );
 }
 
