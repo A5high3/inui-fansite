cf03f728813bd8b05aa71480161fff663bdc443b
ashgray <7ashgray7@gmail.com>
2020-11-03 23:18:46 +0900

    靴の花火追加

--- src/components/embededYoutubeMusic.tsx
+++ src/components/embededYoutubeMusic.tsx
@@ -8,12 +8,18 @@ export default function EmbededYoutubeMusic() {
   return (
     <>
       <div style={{ width: "100%", textAlign: "center", fontSize: 50 }}>
-        <div style={{color: " #7e2c46", fontFamily: "'M PLUS 1p'"}}>
+        <div style={{ color: " #7e2c46", fontFamily: "'M PLUS 1p'" }}>
           <SectionHeader /> M u s i c <SectionHeader />
         </div>
       </div>
       <div style={{ marginRight: 150, marginLeft: 150, marginBottom: 100 }}>
         <Carousel infiniteLoop={true} dynamicHeight={true} showThumbs={false}>
+          <div>
+            <Youtube
+              videoId="CT7tJZwWLdQ"
+              opts={{ height: "600", width: "760" }}
+            />
+          </div>
           <div>
             <Youtube
               videoId="QzdsaXemBWM"
