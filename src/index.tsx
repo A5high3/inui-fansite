022e33f29ae6e2c65f713dbe5a034c564af46de0
watano1168 <watano1168@gmail.com>
2021-03-13 03:43:56 +0900

    [Change] リファクタリング

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,8 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP" rel="stylesheet"></link>
-    <link
-      href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@700"
-      rel="stylesheet"
-    ></link>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@700" rel="stylesheet" />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
